71ce32d158d58919e9d86479005c7260eaf82308
Anaël iMac <anael@barodine.fr>
2021-04-26 14:39:14 +0200

    Rechercher une ressource création composant.

---
+++ src/components/RechercheRessource.vue
@@ -0,0 +1,15 @@
+<template>
+  <div class="cadre-recherche-ressource">
+
+  </div>
+</template>
+
+<script>
+export default {
+  name: "RechercheRessource"
+}
+</script>
+
+<style scoped>
+
+</style>
