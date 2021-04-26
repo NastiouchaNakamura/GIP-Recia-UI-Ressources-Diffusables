665084b6fcd7975a4348ec234c458e457ef250e6
Anaël iMac <anael@barodine.fr>
2021-04-26 16:05:47 +0200

    Responsiveness.

--- src/components/PageRessource.vue
+++ src/components/PageRessource.vue
@@ -1,7 +1,7 @@
 <template>
-  <div class="total">
-    <recherche-ressource v-bind:recherche="recherche" ref="rechercheRessource" class="recherche-ressource"/>
-    <liste-ressources v-bind:recherche="recherche" ref="listeRessource" class="liste-ressource"/>
+  <div class="cadre-page-ressource">
+    <recherche-ressource v-bind:recherche="recherche" ref="rechercheRessource" class="recherche-ressource-page-ressource"/>
+    <liste-ressources v-bind:recherche="recherche" ref="listeRessource" class="liste-ressource-page-ressource"/>
   </div>
 </template>
 
@@ -37,33 +37,39 @@ export default {
 </script>
 
 <style scoped>
-table, th, td {
-  border: 1px solid black;
-}
 
-th, td {
-  min-width: 50px;
-}
-
-table, legend {
-  font-size: 75%;
-  margin-left: auto;
-  margin-right: auto;
-}
-
-.total {
+.cadre-page-ressource {
   display: flex;
+  flex-direction: row;
   margin: 0;
   padding: 0;
 }
 
-.recherche-ressource {
+.recherche-ressource-page-ressource {
   max-width: 20vw;
-  flex-grow: 0;
-}
-
-.liste-ressource {
   width: 100%;
   flex-grow: 0;
+  flex-shrink: 0;
+}
+
+.liste-ressource-page-ressource {
+  max-width: 80vw;
+  width: 100%;
+  flex-grow: 0;
+  flex-shrink: 0;
+}
+
+@media (max-width: 1080px) {
+  .cadre-page-ressource {
+    flex-direction: column;
+  }
+
+  .recherche-ressource-page-ressource {
+    max-width: 100vw;
+  }
+
+  .liste-ressource-page-ressource {
+    max-width: 100vw;
+  }
 }
 </style>
