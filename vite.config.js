11bb7707c1519897e843cffd789047c6ab388622
Quentin Guillemin <guillemin.quentin@outlook.fr>
2023-01-06 16:59:10 +0100

    fix: build project works :tada:

--- vite.config.js
+++ vite.config.js
@@ -4,7 +4,7 @@ import { resolve, dirname } from "node:path";
 import { defineConfig } from "vite";
 import vue from "@vitejs/plugin-vue";
 import legacy from "@vitejs/plugin-legacy";
-import vueI18n from "@intlify/vite-plugin-vue-i18n";
+import VueI18nPlugin from "@intlify/unplugin-vue-i18n/vite";
 
 // https://vitejs.dev/config/
 export default defineConfig({
@@ -14,12 +14,7 @@ export default defineConfig({
       targets: ["ie >= 11"],
       additionalLegacyPolyfills: ["regenerator-runtime/runtime"],
     }),
-    vueI18n({
-      include: resolve(
-        dirname(fileURLToPath(import.meta.url)),
-        "./src/locales/**"
-      ),
-    }),
+    VueI18nPlugin(),
   ],
   resolve: {
     alias: {
