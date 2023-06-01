3cef2d86b965ec9251fe372d9776f3ffef412184
Quentin Guillemin <quentin.guillemin@recia.fr>
2023-06-01 17:44:07 +0200

    feat: add props insted of env variables

--- src/services/serviceRessourcesDiffusables.ts
+++ src/services/serviceRessourcesDiffusables.ts
@@ -1,57 +1,51 @@
 import oidc from "@uportal/open-id-connect";
 import axios from "axios";
 
-const {
-  VITE_BASE_API_URI,
-  VITE_RESSOURCES_DIFFUSABLES_API_URI,
-  VITE_RESSOURCES_DIFFUSABLES_SIZE_API_URI,
-  VITE_USER_INFO_API_URI,
-} = import.meta.env;
-
-async function getToken(): Promise<string | undefined> {
+const getToken = async (
+  userInfoApiUrl: string
+): Promise<string | undefined> => {
   try {
     const { encoded } = await oidc({
-      userInfoApiUrl: VITE_USER_INFO_API_URI,
+      userInfoApiUrl: userInfoApiUrl,
     });
 
     return encoded;
   } catch (e) {
     console.error(e);
   }
-}
+};
 
-function getUrlParams(recherche: string): string {
-  return recherche !== ""
+const getUrlParams = (recherche: string): string =>
+  recherche !== ""
     ? `&operator=OR&idRessource=${recherche}&nomRessource=${recherche}&idEditeur=${recherche}&nomEditeur=${recherche}&distributeurCom=${recherche}&nomDistributeurCom=${recherche}&distributeurTech=${recherche}&nomDistributeurTech=${recherche}`
     : "";
-}
 
-async function getRessourcesDiffusables(page: number, recherche: string) {
-  return await axios.get(
-    `${VITE_BASE_API_URI}${VITE_RESSOURCES_DIFFUSABLES_API_URI}?ressourcesPerPage=20&page=${page}${getUrlParams(
-      recherche
-    )}`,
+const getRessourcesDiffusables = async (
+  url: string,
+  userInfoApiUrl: string,
+  page: number,
+  recherche: string
+) =>
+  await axios.get(
+    `${url}?ressourcesPerPage=20&page=${page}${getUrlParams(recherche)}`,
     {
       headers: {
-        Authorization: `Bearer ${await getToken()}`,
+        Authorization: `Bearer ${await getToken(userInfoApiUrl)}`,
         "content-type": "application/jwt",
       },
     }
   );
-}
 
-async function getSize(recherche: string) {
-  return await axios.get(
-    `${VITE_BASE_API_URI}${VITE_RESSOURCES_DIFFUSABLES_SIZE_API_URI}?${getUrlParams(
-      recherche
-    )}`,
-    {
-      headers: {
-        Authorization: `Bearer ${await getToken()}`,
-        "content-type": "application/jwt",
-      },
-    }
-  );
-}
+const getSize = async (
+  url: string,
+  userInfoApiUrl: string,
+  recherche: string
+) =>
+  await axios.get(`${url}?${getUrlParams(recherche)}`, {
+    headers: {
+      Authorization: `Bearer ${await getToken(userInfoApiUrl)}`,
+      "content-type": "application/jwt",
+    },
+  });
 
 export { getRessourcesDiffusables, getSize };
