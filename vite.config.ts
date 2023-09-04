f0411b644c43c952d1a5e01b08c89c5170e5a211
Quentin Guillemin <quentin.guillemin@recia.fr>
2023-09-04 10:40:40 +0200

    build(deps): bump
    * @intlify/unplugin-vue-i18n from 0.12.3 to 0.13.0
    
    * @types/node from 18.17.12 to 18.17.14
    
    * @vue/tsconfig from 0.1.3 to 0.4.0
    
    * prettier from 3.0.2 to 3.0.3
    
    * terser from 5.19.2 to 5.19.3

--- vite.config.ts
+++ vite.config.ts
@@ -1,43 +1,45 @@
-import VueI18nPlugin from "@intlify/unplugin-vue-i18n/vite";
-import legacy from "@vitejs/plugin-legacy";
-import vue from "@vitejs/plugin-vue";
-import { resolve, dirname } from "node:path";
-import { fileURLToPath } from "node:url";
-import { defineConfig } from "vite";
+import VueI18nPlugin from '@intlify/unplugin-vue-i18n/vite';
+import legacy from '@vitejs/plugin-legacy';
+import vue from '@vitejs/plugin-vue';
+import { dirname, resolve } from 'node:path';
+import { fileURLToPath } from 'node:url';
+import { defineConfig, loadEnv } from 'vite';
 
 // https://vitejs.dev/config/
-export default defineConfig({
-  plugins: [
-    vue({
-      template: {
-        compilerOptions: {
-          isCustomElement: (tag) => tag.includes("-ce"),
+export default ({ mode }: { mode: string }) => {
+  process.env = { ...process.env, ...loadEnv(mode, process.cwd()) };
+
+  return defineConfig({
+    base: process.env.VITE_BASE_URI,
+    plugins: [
+      vue({
+        template: {
+          compilerOptions: {
+            isCustomElement: (tag) => tag.includes('-ce'),
+          },
+        },
+      }),
+      legacy({
+        targets: ['ie >= 11'],
+        additionalLegacyPolyfills: ['regenerator-runtime/runtime'],
+      }),
+      VueI18nPlugin({
+        include: resolve(dirname(fileURLToPath(import.meta.url)), './src/locales/**'),
+      }),
+    ],
+    resolve: {
+      alias: {
+        '@': fileURLToPath(new URL('./src', import.meta.url)),
+      },
+    },
+    build: {
+      rollupOptions: {
+        output: {
+          assetFileNames: 'assets/ui-ressources-gar-[name].[ext]',
+          entryFileNames: 'assets/ui-ressources-gar-[name].js',
+          chunkFileNames: 'assets/ui-ressources-gar-[name].js',
         },
       },
-    }),
-    legacy({
-      targets: ["ie >= 11"],
-      additionalLegacyPolyfills: ["regenerator-runtime/runtime"],
-    }),
-    VueI18nPlugin({
-      include: resolve(
-        dirname(fileURLToPath(import.meta.url)),
-        "./src/locales/**"
-      ),
-    }),
-  ],
-  resolve: {
-    alias: {
-      "@": fileURLToPath(new URL("./src", import.meta.url)),
     },
-  },
-  build: {
-    rollupOptions: {
-      output: {
-        assetFileNames: "assets/ui-ressources-gar-[name].[ext]",
-        entryFileNames: "assets/ui-ressources-gar-[name].js",
-        chunkFileNames: "assets/ui-ressources-gar-[name].js",
-      },
-    },
-  },
-});
+  });
+};
