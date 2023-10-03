73aa35c0eaadebf866d22349314286f35a32a95e
Quentin Guillemin <quentin.guillemin@recia.fr>
2023-10-03 12:14:07 +0200

    refactor: remove unused

--- vite.config.ts
+++ vite.config.ts
@@ -1,6 +1,5 @@
 import VueI18nPlugin from '@intlify/unplugin-vue-i18n/vite';
 import vue from '@vitejs/plugin-vue';
-import { dirname, resolve } from 'node:path';
 import { fileURLToPath } from 'node:url';
 import { defineConfig, loadEnv } from 'vite';
 import cssInjectedByJsPlugin from 'vite-plugin-css-injected-by-js';
@@ -19,9 +18,7 @@ export default ({ mode }: { mode: string }) => {
           },
         },
       }),
-      VueI18nPlugin({
-        include: resolve(dirname(fileURLToPath(import.meta.url)), './src/locales/**'),
-      }),
+      VueI18nPlugin({}),
       cssInjectedByJsPlugin(),
     ],
     resolve: {
